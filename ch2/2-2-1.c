dbdd607dbc31dcb7feb2d65351a2186deb6b94e3
yyysolhhh <solbaram37@gmail.com>
2024-07-15 23:29:22 +0900

    ch2 연습문제 2-2

---
+++ ch2/2-2-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("저의 이름은 홍길동입니다.\n");
+	printf("저의 나이는 %d살이고요.\n", 20);
+	printf("제가 사는 번지수는 %d-%d번지입니다.\n", 111, 222);
+	return 0;
+}
---
+++ ch2/2-2-2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d x %d = %d\n", 2, 3, 6);
+	printf("%d x %d = %d", 2, 4, 8);
+	return 0;
+}
