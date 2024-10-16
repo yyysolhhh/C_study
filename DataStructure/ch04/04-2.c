4f0e8c0d3c3e6df07b074934a198c12650945e3d
yyysolhhh <solbaram37@gmail.com>
2024-10-16 17:30:58 +0900

    chpater04 문제04-1, 04-2

---
+++ DataStructure/ch04/04-2.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node
+{
+	int data;
+	struct _node * next;
+} Node;
+
+int main(void)
+{
+	Node * head = NULL;
+	Node * tail = NULL;
+	Node * cur = NULL;
+	Node * newNode = NULL;
+	int readData;
+
+	head = (Node *)malloc(sizeof(Node));
+	tail = head;
+
+	while (1)
+	{
+		printf("자연수 입력: ");
+		scanf("%d", &readData);
+		if (readData < 1)
+			break;
+
+		newNode = (Node *)malloc(sizeof(Node));
+		newNode->data = readData;
+		newNode->next = NULL;
+
+//		if (head == NULL)
+//			head = newNode;
+//		else
+//			tail->next = newNode;
+//		tail = newNode;
+		tail->next = newNode;
+		tail = newNode;
+	}
+	printf("\n");
+
+	printf("입력 받은 데이터의 전체출력! \n");
+	if (head == tail)
+	{
+		printf("저장된 자연수가 존재하지 않습니다. \n");
+	}
+	else
+	{
+		cur = head;
+		//printf("%d ", cur->data);
+		while (cur->next != NULL)
+		{
+			cur = cur->next;
+			printf("%d ", cur->data);
+		}
+	}
+	printf("\n\n");
+
+	if (head == tail)
+	{
+		return 0;
+	}
+	else
+	{
+		Node * delNode = head;
+		Node * delNextNode = head->next;
+
+//		printf("%d을 삭제합니다 \n", head->data);
+//		free(delNode);
+
+		while (delNextNode != NULL)
+		{
+			delNode = delNextNode;
+			delNextNode = delNextNode->next;
+
+			printf("%d을 삭제합니다\n", delNode->data);
+			free(delNode);
+		}
+	}
+	return 0;
+}
